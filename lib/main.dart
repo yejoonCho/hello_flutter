920c0dccdd33181aaae8c847090471510400343d
yejoon Cho <uactye@LAPTOP-QEMTIR7D.localdomain>
2021-08-28 12:09:24 +0900

    Create basic widget

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "First app",
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First app'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[Text('Hello'), Text('Hello'), Text('Hello')],
+        ),
+      ),
+    );
+  }
+}
